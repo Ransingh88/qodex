cbd561f9b271befc0d26aa036d2ea868d5c442bf
Debasish Ransingh <rajaransingh@gmail.com>
2025-07-30 03:00:06 +0530

    feat: enhance Dashboard and Profile layouts with new styling and back navigation

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useSelector } from "react-redux"
 import { Outlet } from "react-router"
 import Navbar from "@/components/navbar/Navbar"
+import { ArrowLeft, MoveLeft } from "lucide-react"
 
 const DashboardLayout = () => {
   const { user } = useSelector((state) => state.auth)
@@ -10,13 +11,17 @@ const DashboardLayout = () => {
       <Navbar />
       <div className="dashboard-main_container bg-basebg-default text-fg-default">
         <div className="dashboard-container container mx-auto">
-          <div className="mt-24 flex m-2 min-h-[calc(100vh-6.5rem)]">
-            <div className="dashboard-sidebar bg-basebg-default w-1/6 px-4 py-8">
+          <div className="mt-24 flex justify-center items-center m-2 h-[calc(100vh-6.5rem)]">
+            <div className="dashboard-sidebar bg-basebg-default w-1/6 h-full px-4 py-8">
               <p>Profile</p>
               {user.role == "admin" && <p>Dashboard</p>}
               <p>Courses</p>
             </div>
-            <div className="dashboard-content w-5/6 bg-basebg-surface text-fg-default p-4 rounded-lg border border-border-default">
+            <div className="dashboard-content w-5/6 h-full flex flex-col justify-around items-start bg-basebg-surface text-fg-default px-8 py-4 rounded-lg border border-border-default">
+              <button className="flex items-center gap-1 cursor-pointer text-sm my-2 text-fg-default/80 hover:text-fg-muted">
+                <ArrowLeft size={18} className="mt-0.5" />
+                Back
+              </button>
               <Outlet />
             </div>
           </div>
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,16 +1,43 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import "./profile.css"
+import { Pencil } from "lucide-react"
 
 const Profile = () => {
   const { user } = useSelector((state) => state.auth)
   return (
-    <div>
-      <p>Name: {user.fullName}</p>
-      <p>Email: {user.email}</p>
-      <p>Username: {user.username}</p>
-      <p>Role: {user.role}</p>
-      <p>Account Created: {new Date(user.createdAt).toDateString()}</p>
-      <p>lastUpdated:{new Date(user.createdAt).toDateString()}</p>
+    <div className="profile-main_container">
+      <div className="profile-profile_info">
+        <div className="user-info">
+          <div className="user-details">
+            <div className="absolute inset-0 h-full w-full mask-b-from-20% mask-b-to-80% z-0 pointer-events-none opacity-50">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1669411190248-b67e6c4a50cf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIxfHx8ZW58MHx8fHx8"
+                alt=""
+                srcset=""
+                className="h-full w-full bg-cover bg-center"
+              />
+            </div>
+            <div className="user-avatar ">
+              {/* <img src="" alt="" srcset="" /> */}
+              {user?.fullName[0].toUpperCase()}
+            </div>
+            <div className="user-name mb-1 ">
+              <h5>{user.fullName}</h5>
+              <p className="text-sm text-fg-muted">{user.email}</p>
+            </div>
+          </div>
+          <div className="px-4 flex flex-col gap-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button className="flex items-center justify-center h-10 gap-2 cursor-pointer text-sm my-2 text-fg-muted w-full bg-basebg-surface2 rounded hover:bg-basebg-surface hover:border border-border-default">
+              <Pencil size={16} className="mt-0.5" />
+              Edit Profile
+            </button>
+          </div>
+        </div>
+        <div className="h-0.25  bg-border-default m-4"></div>
+      </div>
+      <div className="profile-performance"></div>
     </div>
   )
 }
