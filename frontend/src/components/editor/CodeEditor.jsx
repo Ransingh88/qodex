bcc8f70fb5e65d8332484b0aa4b34d3828c12e6c
Debasish Ransingh <rajaransingh@gmail.com>
2025-08-08 03:43:43 +0530

    feat: add loading state to ProblemDetails and improve conditional rendering

--- frontend/src/components/editor/CodeEditor.jsx
+++ frontend/src/components/editor/CodeEditor.jsx
@@ -2,15 +2,17 @@ import Editor from "@monaco-editor/react"
 
 const CodeEditor = ({ sourceCode, onChange }) => {
   return (
-    <Editor
-      // height="90%"
-      onChange={onChange}
-      theme="vs-dark"
-      defaultLanguage="javascript"
-      defaultValue={sourceCode}
-      options={{ automaticLayout: true, padding: { top: 16, bottom: 16 } }}
-      language="javascript"
-    />
+    sourceCode && (
+      <Editor
+        // height="90%"
+        onChange={onChange}
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        defaultValue={sourceCode}
+        options={{ automaticLayout: true, padding: { top: 16, bottom: 16 } }}
+        language="javascript"
+      />
+    )
   )
 }
 
