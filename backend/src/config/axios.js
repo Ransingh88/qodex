bc3e71bb8da84336ab51fb0e1005718b5c3180b2
Debasish Ransingh <rajaransingh@gmail.com>
2025-11-06 23:47:08 +0530

    feat: update API configuration to use RapidAPI keys and improve CORS settings

--- backend/src/config/axios.js
+++ backend/src/config/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios"
-import { JUDGE0_API_URL, JUDGE0_SULU_API_KEY } from "../config/config.js"
+import { JUDGE0_API_URL, RAPID_API_HOST, RAPID_API_KEY } from "../config/config.js"
 
 export const judge0Axios = axios.create({
   baseURL: JUDGE0_API_URL,
   headers: {
-    Authorization: `Bearer ${JUDGE0_SULU_API_KEY}`,
+    "x-rapidapi-key": `${RAPID_API_KEY}`,
+    "x-rapidapi-host": `${RAPID_API_HOST}`,
     "Content-Type": "application/json",
+    // Authorization: `Bearer ${JUDGE0_API_KEY}`,
   },
 })
